7347b13abcfc730bbb5ae8304b196ee07f15b144
Suraj Yadav <surajyaduvansi202050@gmail.com>
2026-08-25 16:38:39 +0530

    Add edit and cancel expense feature

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ const expensesContainer = document.getElementById("expenses");
 
 let addExpense = document.getElementById('addExpense');
 
-let  clearExpenses = document.getElementById("clearExpenses");
+let editingExpenseId = null;
+
+const cancelEdit = document.getElementById("cancelEdit");
+
+const editMessage = document.getElementById("editMessage");
+
+let clearExpenses = document.getElementById("clearExpenses");
 
 const filterCategory = document.getElementById("filterCategory");
 
@@ -22,26 +28,26 @@ let expenses = JSON.parse(localStorage.getItem('expenses')) || [];
 function getFilteredExpenses() {
     let filteredExpenses = expenses;
 
-    if(filterCategory.value !== "All") {
-        filteredExpenses = filteredExpenses.filter(function(expense){
+    if (filterCategory.value !== "All") {
+        filteredExpenses = filteredExpenses.filter(function (expense) {
             return expense.category === filterCategory.value;
         });
-    } 
+    }
 
     const searchText = searchExpense.value.toLowerCase();
 
-    filteredExpenses = filteredExpenses.filter(function(expense) {
+    filteredExpenses = filteredExpenses.filter(function (expense) {
         return expense.title.toLowerCase().includes(searchText);
     });
 
-    if(sortExpenses.value === "latest"){
-        filteredExpenses.sort(function(a,b){
+    if (sortExpenses.value === "latest") {
+        filteredExpenses.sort(function (a, b) {
             return new Date(b.date) - new Date(a.date)
         });
     }
 
-    if(sortExpenses.value === "oldest"){
-        filteredExpenses.sort(function(a,b){
+    if (sortExpenses.value === "oldest") {
+        filteredExpenses.sort(function (a, b) {
             return new Date(a.date) - new Date(b.date);
         });
     }
@@ -54,16 +60,16 @@ function updateTotal() {
 
     const filteredExpenses = getFilteredExpenses();
 
-    const total = filteredExpenses.reduce(function(sum, expense) {
+    const total = filteredExpenses.reduce(function (sum, expense) {
         return sum + Number(expense.amount);
     }, 0);
 
     document.getElementById("totalExpense").textContent = `₹${total}`;
 }
 
-function saveExpenses(){
+function saveExpenses() {
     localStorage.setItem("expenses", JSON.stringify(expenses));
-      console.log("Saved:", localStorage.getItem("expenses"));
+    console.log("Saved:", localStorage.getItem("expenses"));
 }
 
 function renderExpenses() {
@@ -72,12 +78,12 @@ function renderExpenses() {
 
     const filteredExpenses = getFilteredExpenses();
 
- if(filteredExpenses.length === 0){
-    expensesContainer.innerHTML = "<p>No expenses found</p>"
-    return;
- }
+    if (filteredExpenses.length === 0) {
+        expensesContainer.innerHTML = "<p>No expenses found</p>"
+        return;
+    }
 
-    filteredExpenses.forEach(function(expense)  {
+    filteredExpenses.forEach(function (expense) {
 
         const expenseElement = document.createElement("div");
         expenseElement.classList.add('expense-card')
@@ -99,11 +105,24 @@ function renderExpenses() {
         const editButton = expenseElement.querySelector(".edit-btn");
         const deleteButton = expenseElement.querySelector(".delete-btn");
 
-        deleteButton.addEventListener("click", function() {
+        editButton.addEventListener("click", function () {
+            titleInput.value = expense.title;
+            amountInput.value = expense.amount;
+            categoryInput.value = expense.category;
+
+            editingExpenseId = expense.id;
+            addExpense.textContent = "Update Expense";
+
+            cancelEdit.style.display = "inline-block";
+            editMessage.style.display = "block";
+        })
+
+
+        deleteButton.addEventListener("click", function () {
 
             const id = Number(deleteButton.dataset.id);
 
-            const expenseIndex = expenses.findIndex(function(expense){
+            const expenseIndex = expenses.findIndex(function (expense) {
                 return expense.id === id;
             })
 
@@ -119,38 +138,79 @@ function renderExpenses() {
     });
 }
 
-filterCategory.addEventListener("change", function() {
+cancelEdit.addEventListener("click", function () {
+
+    titleInput.value = "";
+    amountInput.value = "";
+    categoryInput.value = "Food";
+
+    editingExpenseId = null;
+
+    addExpense.textContent = "Add Expense";
+    cancelEdit.style.display = "none";
+    editMessage.style.display = "none";
+
+});
+
+filterCategory.addEventListener("change", function () {
     renderExpenses();
     updateTotal();
 });
 
-sortExpenses.addEventListener("change", function(){
+sortExpenses.addEventListener("change", function () {
     renderExpenses();
     updateTotal();
 })
 
 
- searchExpense.addEventListener("input", function() {
+searchExpense.addEventListener("input", function () {
     renderExpenses();
     updateTotal();
 });
 
 addExpense.addEventListener('click', () => {
 
-    if(
+    if (
         titleInput.value.trim() == "" ||
-         amountInput.value.trim() == ""
-        ){
+        amountInput.value.trim() == ""
+    ) {
         alert("Please fill all fields")
         return;
     }
 
     const amount = Number(amountInput.value);
 
-    if(isNaN(amount) || amount <= 0){
+    if (isNaN(amount) || amount <= 0) {
         alert("Please enter the valid amount");
         return;
     }
+
+    if (editingExpenseId !== null) {
+        const expenseIndex = expenses.findIndex(function (expense) {
+            return expense.id === editingExpenseId;
+        });
+
+        expenses[expenseIndex].title = titleInput.value;
+        expenses[expenseIndex].amount = amount;
+        expenses[expenseIndex].category = categoryInput.value;
+
+        editingExpenseId = null;
+        addExpense.textContent = "Add Expense";
+
+        titleInput.value = "";
+        amountInput.value = "";
+        categoryInput.value = "Food";
+        cancelEdit.style.display = "none";
+        editMessage.style.display = "none";
+
+        saveExpenses();
+        renderExpenses();
+        updateTotal();
+
+        return;
+    }
+
+
 
     const expense = {
         id: Date.now(),
@@ -159,7 +219,7 @@ addExpense.addEventListener('click', () => {
         category: categoryInput.value,
         date: new Date().toISOString()
     }
-   
+
     expenses.push(expense);
 
     titleInput.value = "";
@@ -171,14 +231,14 @@ addExpense.addEventListener('click', () => {
     renderExpenses();
     updateTotal();
 
-   
-    
+
+
 })
 
 clearExpenses.addEventListener("click", () => {
     const confirmClear = confirm("Are you aure you want to delete all expenses?");
 
-    if(!confirmClear){
+    if (!confirmClear) {
         return;
     }
 
