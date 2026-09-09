630c976966d69d4f42b00ffdef422886c1752719
Suraj Yadav <surajyaduvansi202050@gmail.com>
2026-09-09 22:32:52 +0530

    Add user authentication setup

--- Backend/server.js
+++ Backend/server.js
@@ -3,6 +3,9 @@ const cors = require("cors");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const Expense = require("./models/Expense");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 
 const app = express();
@@ -38,6 +41,78 @@ app.get("/expenses", async (req, res) => {
 
         res.status(500).json({
             message: "Failed to fetch expenses"
+        });
+    }
+});
+
+app.post("/register", async (req, res) => {
+    try{
+        const {name, email, password} = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const safeUser = user.toObject();
+        delete safeUser.password;
+
+        res.status(201).json({
+            message: "User register successfully",
+            user: safeUser
+        });
+
+    }catch(error){
+        console.log(error);
+
+        res.status(500).json({
+            message: "Failed to register user"
+        });
+    }
+});
+
+app.post("/login", async (req, res) => {
+    try{
+        const {email, password} = req.body;
+
+        const user = await User.findOne({ email: email });
+
+        if(!user){
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if(!isPasswordCorrect){
+            return res.status(401).json({
+                message: "Invalid password"
+            });
+        }
+
+        const token = jwt.sign(
+            {userId: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: "7d"}
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token
+        });
+
+    }catch(error){
+        console.log(erro)
+
+        res.status(500).json({
+            message: "Failed to login "
         });
     }
 });
